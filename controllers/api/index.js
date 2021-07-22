7041a6b8dd1d09b2f05e1bd5c88fe2cced4e2e36
Kelley Worthington <kelleymarne@gmail.com>
2021-07-22 05:21:55 -0600

    login/logout

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,9 +4,8 @@ const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-route');
 
-
 router.use('/users', userRoutes);
-router.use('/posts', postRoutes)
+router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
 module.exports = router;
